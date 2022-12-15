78e7f7bc412d112ae83b77c1539e4555bf15400c
maxarmy <dinkomax@gmail.com>
2022-12-14 16:02:37 -0800

    corrections 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,12 +9,14 @@ void times_table(void)
 {
 	int a;
 	int b;
+	int c;
 
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			_putchar(a*b);
+			c = a * b;
+			_putchar(c);
 		}
 		_putchar('\n');
 	}
