e130953948cbc3c8e39cec7a36c2ec76e903ea7f
maxarmy <dinkomax@gmail.com>
2023-01-12 15:30:30 -0800

    0 and 1 redo

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,10 +11,10 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *p;
 
-	ptr = malloc(b);
-	if (ptr == NULL)
+	p = malloc(b);
+	if (p == NULL)
 		exit(98);
-	return (ptr);
+	return (p);
 }
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,30 +13,31 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 {
-	char *ptr;
-	int x, y;
+	char *strDup;
+	int i;
+	unsigned int j;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[x] != '\0')
-		x++;
-	ptr = malloc(sizeof(char) * (x + n + 1));
-	x = 0;
-	y = 0;
-	if (ptr = NULL)
-		return (NULL)
-	while (s1[x] != '\0')
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	strDup = malloc(sizeof(char) * (i + n + 1));
+	if (strDup == NULL)
+		return (NULL);
+	i = j = 0;
+	while (s1[i] != '\0')
 	{
-		ptr[x] = s1[x];
-		x++;
+		strDup[i] = s1[i];
+		i++;
 	}
-	while (y < n && s2[y] != '\0')
+	while (j < n && s2[j] != '\0')
 	{
-		ptr[x] = s2[y];
-		x++, y++;
+		strDup[i] = s2[j];
+		i++, j++;
 	}
-	ptr[x] = '\0';
-	return (ptr);
+	strDup[i] = '\0';
+	return (strDup);
 }
